45e5fa40d2ea16b31af54b58a194156ce15113b0
jung0930 <wjddlqkqh57@naver.com>
2021-10-13 10:17:21 +0900

    test : 거리간 비교를 하는지 테스트

--- src/test/java/racinggame/domain/DistanceTest.java
+++ src/test/java/racinggame/domain/DistanceTest.java
@@ -20,4 +20,31 @@ class DistanceTest {
         assertThat(distance).isEqualTo(Distance.from(6));
     }
 
+    @DisplayName("거리간 비교하여 같으면 0을 반환한다.")
+    @Test
+    void compare_same() {
+        Distance distance1 = Distance.from(2);
+        Distance distance2 = Distance.from(2);
+
+        assertThat(distance1.compare(distance2)).isEqualTo(0);
+    }
+
+    @DisplayName("거리간 비교하여 작으면 -1를 반환한다.")
+    @Test
+    void compare_smaller() {
+        Distance distance1 = Distance.from(2);
+        Distance distance2 = Distance.from(3);
+
+        assertThat(distance1.compare(distance2)).isEqualTo(-1);
+    }
+
+    @DisplayName("거리간 비교하여 크면 1을 반환한다.")
+    @Test
+    void compare_bigger() {
+        Distance distance1 = Distance.from(2);
+        Distance distance2 = Distance.from(1);
+
+        assertThat(distance1.compare(distance2)).isEqualTo(1);
+    }
+
 }
