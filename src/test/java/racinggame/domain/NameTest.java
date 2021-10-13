48367df7bc783c21d87788bacef86864c112aeba
jung0930 <wjddlqkqh57@naver.com>
2021-10-13 09:46:30 +0900

    test : 값이 비었거나 Null인지 체크하는지 테스트

---
+++ src/test/java/racinggame/domain/NameTest.java
@@ -0,0 +1,27 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SuppressWarnings("NonAsciiCharacters")
+class NameTest {
+
+    @DisplayName("생성")
+    @Test
+    void create() {
+        assertThat(Name.from("abcde")).isInstanceOf(Name.class);
+    }
+
+    @DisplayName("빈값일 경우 exception 발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void validate_빈값(String input) {
+        assertThatIllegalArgumentException().isThrownBy(() -> Name.from(input));
+    }
+
+}
