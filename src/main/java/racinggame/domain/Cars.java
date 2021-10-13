ffd7f99fcacb0cc705234645f1f934a17cc048db
jung0930 <wjddlqkqh57@naver.com>
2021-10-13 10:30:26 +0900

    feat : Names를 이용해 Cars 생성

---
+++ src/main/java/racinggame/domain/Cars.java
@@ -0,0 +1,50 @@
+package racinggame.domain;
+
+import racinggame.domain.strategy.MovableStrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Cars {
+
+    private final List<Car> cars;
+
+    private Cars(final List<Car> cars) {
+        this.cars = cars;
+    }
+
+    private Cars(final Names names) {
+        this.cars = convertCarNames(names);
+    }
+
+    public static Cars from(final Names names) {
+        return new Cars(names);
+    }
+
+    private List<Car> convertCarNames(final Names names) {
+        List<Car> cars = new ArrayList<>();
+        for (Name name : names.value()) {
+            cars.add(Car.from(name));
+        }
+        return cars;
+    }
+
+    public List<Car> value() {
+        return cars;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cars cars1 = (Cars) o;
+        return Objects.equals(cars, cars1.cars);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cars);
+    }
+
+}
