07ea3b14d4cc8750e8f33fc34716b1cdd121daa7
jung0930 <wjddlqkqh57@naver.com>
2021-10-13 13:09:31 +0900

    feat : 컨트롤러와 UI단 구현

---
+++ src/main/java/racinggame/controller/RacingCarController.java
@@ -0,0 +1,32 @@
+package racinggame.controller;
+
+import racinggame.domain.Cars;
+import racinggame.domain.Names;
+import racinggame.domain.RacingGame;
+import racinggame.domain.TryCount;
+import racinggame.domain.strategy.RacingMovableStrategy;
+import racinggame.view.InputView;
+import racinggame.view.OutputView;
+
+public class RacingCarController {
+
+    public void play() {
+        try {
+            start();
+        } catch (IllegalArgumentException e) {
+            OutputView.printErrorMessage(e.getMessage());
+            start();
+        }
+    }
+
+    public void start() {
+        Cars cars = Cars.from(Names.from(InputView.inputCarNames()));
+        TryCount tryCount = TryCount.from(Integer.parseInt(InputView.inputTryCount()));
+
+        RacingGame racingGame = RacingGame.of(cars, tryCount, new RacingMovableStrategy());
+        OutputView.printExecutionResultText();
+        OutputView.printRaces(racingGame);
+        OutputView.printWinningCars(racingGame);
+    }
+
+}
---
+++ src/main/java/racinggame/view/OutputView.java
@@ -0,0 +1,54 @@
+package racinggame.view;
+
+import racinggame.domain.Car;
+import racinggame.domain.RacingGame;
+import racinggame.domain.Round;
+
+import java.util.List;
+
+public final class OutputView {
+
+    private OutputView() {
+    }
+
+    public static void printErrorMessage(final String message) {
+        System.out.println(message + "\n");
+    }
+
+    public static void printExecutionResultText() {
+        System.out.println("\n실행결과");
+    }
+
+    public static void printRaces(final RacingGame racingGame) {
+        for (int i = 0; i < racingGame.getTryCount(); i++) {
+            OutputView.printRace(racingGame.getRace(i));
+        }
+    }
+
+    private static void printRace(final Round round) {
+        List<Car> cars = round.cars().value();
+        for (Car car : cars) {
+            System.out.printf("%s : %s\n", car.name(), printDistance(car.distance()));
+        }
+        System.out.println();
+    }
+
+    private static String printDistance(final int addCount) {
+        StringBuilder distance = new StringBuilder();
+        for (int i = 0; i < addCount; i++) {
+            distance.append("-");
+        }
+        return distance.toString();
+    }
+
+    public static void printWinningCars(final RacingGame racingGame) {
+        StringBuilder winners = new StringBuilder();
+        List<Car> cars = racingGame.findWinningCars().value();
+        for (int i = 0; i < cars.size() - 1; i++) {
+            winners.append(cars.get(i).name()).append(",");
+        }
+        winners.append(cars.get(cars.size() - 1).name());
+        System.out.printf("최종 우승자는 %s 입니다.", winners.toString());
+    }
+
+}
