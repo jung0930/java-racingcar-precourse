581a9295d1993f4e0d5f1776118a57e06f02ee1d
jung0930 <wjddlqkqh57@naver.com>
2021-10-13 09:53:12 +0900

    test : 이름이 두개 이상인지 검사하는지 테스트

---
+++ src/test/java/racinggame/domain/NamesTest.java
@@ -0,0 +1,26 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SuppressWarnings("NonAsciiCharacters")
+class NamesTest {
+
+    @DisplayName("생성")
+    @Test
+    void create() {
+        assertThat(Names.from("a,b,c,d")).isInstanceOf(Names.class);
+    }
+
+    @DisplayName("이름이 두개 미만이면 에러")
+    @Test
+    void validate_이름이_두개_미만() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Names.from("a");
+        });
+    }
+
+}
